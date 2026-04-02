6e3a5a6bb741dd626bf8fed5f3715da27b02e1b8
S.M. Mohibullah Bukhari <mohibullahbukhari98@gmail.com>
2026-04-02 22:53:56 +0500

    chore(config): disable analytics and bump angular deps
    disable analytics and bump angular dependencies
    
    - set cli.analytics to false in angular.json
    - upgrade angular packages to v14.3 and add material/cdk
    - bump rxjs to match new Angular versions
    - refactor converter date handling to use onDateChange
    - update template to call the new handler

--- src/app/features/converter/converter.component.ts
+++ src/app/features/converter/converter.component.ts
@@ -79,4 +79,14 @@ export class ConverterComponent implements OnInit {
       this.history = JSON.parse(data);
     }
   }
+
+  onDateChange(event: any): void {
+    const d = event.value;
+    if (d) {
+      const year = d.getFullYear();
+      const month = String(d.getMonth() + 1).padStart(2, '0');
+      const day = String(d.getDate()).padStart(2, '0');
+      this.date = `${year}-${month}-${day}`;
+    }
+  }
 }
